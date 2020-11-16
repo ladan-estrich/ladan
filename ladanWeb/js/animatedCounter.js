be57aa9d545b72eda57954155a4f2c96bcda8b0a
Marko Filipovic <markofilipovic@Markos-MacBook-Pro.local>
2020-11-16 18:19:51 +0100

    ToDo style about-section - fill with text - style animated counter boxes

--- ladanWeb/js/animatedCounter.js
+++ ladanWeb/js/animatedCounter.js
@@ -1,4 +1,5 @@
 function animateValue(obj, start, end, duration) {
+    // let str = "m2";
     let startTimestamp = null;
     const step = (timestamp) => {
         if (!startTimestamp) startTimestamp = timestamp;
@@ -12,10 +13,12 @@ function animateValue(obj, start, end, duration) {
 }
 
 // const obj = document.getElementsByClassName("value");
+// changed to getElementById due to diversity of use
 const obj = document.getElementById("value");
 const obj2 = document.getElementById("value1");
 const obj3 = document.getElementById("value2");
 
-animateValue(obj, 0, 65000, 3000);
+animateValue(obj, 0, 0, 3000);
 animateValue(obj2, 0, 65000, 3000);
-animateValue(obj3, 0, 65000, 3000);
+animateValue(obj3, 0, 250, 3000);
+
